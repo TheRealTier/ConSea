1b18000bd1c38d076580722193c9283d1ce81541
unknown <sesulzba@OLEW5832.ewenet.ewe.de>
2015-06-23 13:22:12 +0200

    Working SAP Request
    added sampleAction to Eclipse Menu and eventlistener.
    added working SAP Backend Call from withing sampleMenu event
    Request target is still invalid

--- org.consea/src/org/consea/Activator.java
+++ org.consea/src/org/consea/Activator.java
@@ -1,8 +1,6 @@
 package org.consea;
 
-import java.net.URI;
 
-import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -11,14 +9,7 @@ import org.eclipse.ui.PlatformUI;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import com.sap.adt.communication.message.IResponse;
-import com.sap.adt.communication.resources.AdtRestResourceFactory;
-import com.sap.adt.communication.resources.IRestResource;
-import com.sap.adt.communication.resources.IRestResourceFactory;
-import com.sap.adt.communication.resources.ResourceNotFoundException;
-import com.sap.adt.destinations.ui.logon.AdtLogonServiceUIFactory;
-import com.sap.adt.tools.core.project.AdtProjectServiceFactory;
-import com.sap.adt.tools.core.project.IAbapProject;
+
 
 public class Activator implements BundleActivator {
 
@@ -28,50 +19,12 @@ public class Activator implements BundleActivator {
 		return context;
 	}
 	
-	
-	private static final String SAMPLE_FLIGHT_RESOURCE_URI = "/consea/discovery";
-	
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		System.out.println("------- Initialisation ConSea-----------------");
-		System.out.println("PHP 1");
-		
-		IRestResourceFactory restResourceFactory = AdtRestResourceFactory
-				.createRestResourceFactory();
-		System.out.println("PHP 2");
-		IProject[] abapProjects = AdtProjectServiceFactory
-				.createProjectService().getAvailableAbapProjects();
-		System.out.println("PHP 3");
-		IAbapProject abapProject = (IAbapProject) abapProjects[0]
-				.getAdapter(IAbapProject.class);
-		System.out.println("PHP 4");
-		AdtLogonServiceUIFactory.createLogonServiceUI().ensureLoggedOn(
-				abapProject.getDestinationData(),
-				PlatformUI.getWorkbench().getProgressService());
-		System.out.println("PHP 5");
-		String destination = abapProject.getDestinationId();
-		
-		
-		
-		URI flightUri = URI.create(SAMPLE_FLIGHT_RESOURCE_URI);
-		System.out.println(destination);
-		IRestResource flightResource = restResourceFactory
-				.createResourceWithStatelessSession(flightUri, destination);
-		System.out.println("PHP 7");
-		try {
-			
-			IResponse response = flightResource.get(null, IResponse.class);
-			System.out.println("Request erfolgreich:" + response.getStatus() );
-			
-		} catch (ResourceNotFoundException e) {
-			System.out.println("Error");	
-		} catch (RuntimeException e) {
-			System.out.println(e.getMessage());
-		}
 		
 	}
 
--- org.consea/src/org/consea/ConseaHandler.java
+++ org.consea/src/org/consea/ConseaHandler.java
@@ -1,24 +1,9 @@
 package org.consea;
 
-import java.net.URI;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
 
-import com.sap.adt.communication.message.IResponse;
-import com.sap.adt.communication.resources.AdtRestResourceFactory;
-import com.sap.adt.communication.resources.IRestResource;
-import com.sap.adt.communication.resources.IRestResourceFactory;
-import com.sap.adt.communication.resources.ResourceNotFoundException;
-import com.sap.adt.destinations.ui.logon.AdtLogonServiceUIFactory;
-import com.sap.adt.tools.core.project.AdtProjectServiceFactory;
-import com.sap.adt.tools.core.project.IAbapProject;
 
 public class ConseaHandler extends AbstractHandler {
 
@@ -26,56 +11,10 @@ public class ConseaHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		openDialogWindow("Test","Test");
-		
-		IRestResourceFactory restResourceFactory = AdtRestResourceFactory
-				.createRestResourceFactory();
-		IProject[] abapProjects = AdtProjectServiceFactory
-				.createProjectService().getAvailableAbapProjects();
-
-		IAbapProject abapProject = (IAbapProject) abapProjects[0]
-				.getAdapter(IAbapProject.class);
-
-		AdtLogonServiceUIFactory.createLogonServiceUI().ensureLoggedOn(
-				abapProject.getDestinationData(),
-				PlatformUI.getWorkbench().getProgressService());
-
-		String destination = abapProject.getDestinationId();
-		URI flightUri = URI.create(SAMPLE_FLIGHT_RESOURCE_URI);
-		IRestResource flightResource = restResourceFactory
-				.createResourceWithStatelessSession(flightUri, destination);
-		try {
-			IResponse response = flightResource.get(null, IResponse.class);
-			openDialogWindow(
-					"Flight exists! HTTP - status:"
-							+ String.valueOf(response.getStatus()),
-					"Flight Confirmation");
-		} catch (ResourceNotFoundException e) {
-			displayError("No flight data found");
-		} catch (RuntimeException e) {
-			displayError(e.getMessage());
-		}
 		return null;
 	}
 
-	private void displayError(String messageText) {
-		String dialogTitle = "Flight Exception";
-		openDialogWindow(messageText, dialogTitle);
-	}
-
-	protected void openDialogWindow(String dialogText, String dialogTitle) {
-		String[] DIALOG_BUTTON_LABELS = new String[] { IDialogConstants.OK_LABEL };
-		MessageDialog dialog = new MessageDialog(getShell(), dialogTitle, null,
-				dialogText, MessageDialog.INFORMATION, DIALOG_BUTTON_LABELS, 0);
-		dialog.open();
-	}
-
-	protected Shell getShell() {
-		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-				.getShell();
-		return shell;
-	}
+	
 	
 	
 }
