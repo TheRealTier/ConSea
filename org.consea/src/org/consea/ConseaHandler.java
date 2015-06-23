cdb0cee2082f6e60c1c8694a641d01ae51304495
unknown <sesulzba@OLEW5832.ewenet.ewe.de>
2015-06-23 12:32:28 +0200

    Latest

--- org.consea/src/org/consea/ConseaHandler.java
+++ org.consea/src/org/consea/ConseaHandler.java
@@ -2,14 +2,14 @@ package org.consea;
 
 import java.net.URI;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
-//import org.eclipse.ui.PlatformUI;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
 
 import com.sap.adt.communication.message.IResponse;
 import com.sap.adt.communication.resources.AdtRestResourceFactory;
@@ -20,80 +20,62 @@ import com.sap.adt.destinations.ui.logon.AdtLogonServiceUIFactory;
 import com.sap.adt.tools.core.project.AdtProjectServiceFactory;
 import com.sap.adt.tools.core.project.IAbapProject;
 
-public class Activator implements BundleActivator {
+public class ConseaHandler extends AbstractHandler {
 
-	private static BundleContext context;
+	private static final String SAMPLE_FLIGHT_RESOURCE_URI = "/consea/rest_consea";
 
-	static BundleContext getContext() {
-		return context;
-	}
-	
-	
-	private static final String SAMPLE_FLIGHT_RESOURCE_URI = "/consea/discovery";
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-		System.out.println("------- Initialisation ConSea-----------------");
-		System.out.println("PHP 1");
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		openDialogWindow("Test","Test");
 		
 		IRestResourceFactory restResourceFactory = AdtRestResourceFactory
 				.createRestResourceFactory();
-		System.out.println("PHP 2");
 		IProject[] abapProjects = AdtProjectServiceFactory
 				.createProjectService().getAvailableAbapProjects();
-		System.out.println("PHP 3");
+
 		IAbapProject abapProject = (IAbapProject) abapProjects[0]
 				.getAdapter(IAbapProject.class);
-		System.out.println("PHP 4");
+
 		AdtLogonServiceUIFactory.createLogonServiceUI().ensureLoggedOn(
 				abapProject.getDestinationData(),
 				PlatformUI.getWorkbench().getProgressService());
-		System.out.println("PHP 5");
+
 		String destination = abapProject.getDestinationId();
-		
-		
-		
 		URI flightUri = URI.create(SAMPLE_FLIGHT_RESOURCE_URI);
-		System.out.println(destination);
 		IRestResource flightResource = restResourceFactory
 				.createResourceWithStatelessSession(flightUri, destination);
-		System.out.println("PHP 7");
 		try {
-			
 			IResponse response = flightResource.get(null, IResponse.class);
-			System.out.println("Request erfolgreich:" + response.getStatus() );
-			
+			openDialogWindow(
+					"Flight exists! HTTP - status:"
+							+ String.valueOf(response.getStatus()),
+					"Flight Confirmation");
 		} catch (ResourceNotFoundException e) {
-			System.out.println("Error");	
+			displayError("No flight data found");
 		} catch (RuntimeException e) {
-			System.out.println(e.getMessage());
+			displayError(e.getMessage());
 		}
-		
+		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+	private void displayError(String messageText) {
+		String dialogTitle = "Flight Exception";
+		openDialogWindow(messageText, dialogTitle);
 	}
-	
-	
+
 	protected void openDialogWindow(String dialogText, String dialogTitle) {
 		String[] DIALOG_BUTTON_LABELS = new String[] { IDialogConstants.OK_LABEL };
 		MessageDialog dialog = new MessageDialog(getShell(), dialogTitle, null,
 				dialogText, MessageDialog.INFORMATION, DIALOG_BUTTON_LABELS, 0);
 		dialog.open();
 	}
-	
+
 	protected Shell getShell() {
-		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+				.getShell();
 		return shell;
 	}
-
+	
+	
 }
